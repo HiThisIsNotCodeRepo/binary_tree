1f07f3615d7a7be7a77362ab92badaed83196860
qinchenfeng <freddy.qin@gmail.com>
2023-12-18 21:14:18 +0800

    Update

---
+++ 01_Traverse_A_Tree/03_Binary_Tree_Inorder_Traversal/iterative/qns.cpp
@@ -0,0 +1,49 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode *root) {
+        if (root == nullptr) {
+            return {};
+        }
+        vector<int> ans;
+        vector<TreeNode *> stack;
+        stack.push_back(root);
+        while (!stack.empty()) {
+            auto top = stack.back();
+            if (top->left == nullptr) {
+                stack.pop_back();
+                ans.push_back(top->val);
+                if (top->right != nullptr) {
+                    stack.push_back(top->right);
+                }
+            } else {
+                stack.push_back(top->left);
+                top->left = nullptr;
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+
+
+    return 0;
+}
